6513615b4543bdf8c542f4aec5aad0ab46feaaad
Fabio Pugliese Ornellas <fabio.ornellas@gmail.com>
2013-05-08 11:54:05 -0300

    Fixed pointer position detection.

--- Keyboard/EnduranceEEPROM.cpp
+++ Keyboard/EnduranceEEPROM.cpp
@@ -2,16 +2,16 @@
 
 #include <avr/eeprom.h>
 
-#define addrPointer(p) (baseAddr+p)
-#define addrValue(p) (baseAddr+locations+p)
+#define addrPointer(p) (baseAddr+(uint16_t)p)
+#define addrValue(p) (baseAddr+(uint16_t)locations+(uint16_t)p)
 #define readPointer(p) eeprom_read_byte((const uint8_t *)addrPointer(p))
 
-EnduranceEEPROM::EnduranceEEPROM(uint8_t locations, const uint8_t *baseAddr){
+EnduranceEEPROM::EnduranceEEPROM(uint8_t locations, uint8_t *baseAddr){
   this->locations=locations;
   this->baseAddr=baseAddr;
   if(locations==256)
     locations=255;
-  for(pointer=0;(readPointer(getNextPointer())-readPointer(pointer)==1)&&pointer<locations;pointer++);
+  loadPointer();
 }
 
 uint8_t EnduranceEEPROM::getNextPointer(){
@@ -22,12 +22,31 @@ uint8_t EnduranceEEPROM::getNextPointer(){
     return nextPointer;
 }
 
+void EnduranceEEPROM::loadPointer(){
+  for(pointer=0;pointer<locations;pointer++){
+    uint8_t nextPointerValue=readPointer(getNextPointer());
+    uint8_t currPointerValue=readPointer(pointer);
+    if(nextPointerValue>currPointerValue){
+      if(nextPointerValue-currPointerValue!=1)
+        break;
+    }else{
+      if(nextPointerValue==currPointerValue)
+        break;
+      else
+        if(((255-currPointerValue)+(nextPointerValue)+1)!=1)
+          break;
+    }
+  }
+  if(pointer==locations)
+    --pointer;
+}
+
 uint8_t EnduranceEEPROM::read(){
   return eeprom_read_byte((const uint8_t *)addrValue(pointer));
 }
 
 void EnduranceEEPROM::write(uint8_t value){
-  uint8_t nextPointer=getNextPointer();;
+  uint8_t nextPointer=getNextPointer();
   eeprom_write_byte((uint8_t *)addrValue(nextPointer), value);
   eeprom_write_byte((uint8_t *)addrPointer(nextPointer), readPointer(pointer)+1);
   pointer=nextPointer;
--- Keyboard/EnduranceEEPROM.h
+++ Keyboard/EnduranceEEPROM.h
@@ -1,3 +1,6 @@
+#ifndef EnduranceEEPROM_h
+#define EnduranceEEPROM_h
+
 #include <inttypes.h>
 
 class EnduranceEEPROM {
@@ -6,9 +9,12 @@ class EnduranceEEPROM {
     const uint8_t *baseAddr;
     uint8_t pointer;
     uint8_t getNextPointer();
+    void loadPointer();
   public:
     // locations max = 255
-    EnduranceEEPROM(uint8_t locations, const uint8_t *baseAddr);
+    EnduranceEEPROM(uint8_t locations, uint8_t *baseAddr);
     uint8_t read();
     void write(uint8_t value);
 };
+
+#endif
